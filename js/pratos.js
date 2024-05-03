a042472f7c90aae08067ff04088707c81beea93e
Giulianno Sande Monteiro <91056214+Giusande@users.noreply.github.com>
2024-05-03 19:20:20 -0300

    Add files via upload

---
+++ js/pratos.js
@@ -0,0 +1,145 @@
+const pratos = [
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (1).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (2).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (3).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (4).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (5).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (6).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (7).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (8).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (9).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (10).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (11).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (12).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (13).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (14).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (15).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (16).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (17).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (18).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (19).avif",
+        preco: "",
+        peso: ""
+    },
+    {
+        nome: "",
+        descricao: "",
+        imagem: "../imagens/pratos (20).avif",
+        preco: "",
+        peso: ""
+    },
+    
+]
+
+export default pratos;
---
+++ js/pratosContent.js
@@ -0,0 +1,9 @@
+import pratos from "./pratos.js";
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    pratos.forEach(pra => {
+        
+    });
+
+})
